5538b9ff93b97ca712b39524042553b1c5a4ecab
Rospars Quentin <Quentin.Rospars@etu.univ-grenoble-alpes.fr>
2018-04-13 12:48:16 +0200

    Segmentation fibre

--- Model/preTraitementImg.py
+++ Model/preTraitementImg.py
@@ -36,12 +36,12 @@ def HPF(img,n):
     '''
     High-pass filters aide à trouver les edges dans une image
     @param img: image à traiter (créer précédemment grâce à "imread()")
-    @param n:  numéro du filtre appliqué (1 = basique)
+    @param n:  Pourcentage (0 < n < 100) du filtre
     @return: -1 si il y a eu une erreur, sinon l'image après filtrage
     '''
-    if (n==1):
+    if (n>=0 and n<=100):
         HPF1 = -1 * np.ones((5, 5))
-        HPF1[3][3] = 25
+        HPF1[3][3] = 25*(n/100)
         return cv2.filter2D(img,-1,HPF1)
     else:
         print('Appel HPF : Numéro filtre incorrect')
@@ -75,3 +75,18 @@ def BLF(img,n):
         print('Appel BLF : Numéro filtre incorrect')
         return -1
 
+def contrastAndBrightness(img,alpha,beta):
+    '''
+    Augmente / Diminue le contrast et le brightness de l'image
+    @param img: image à traiter (créer précédemment grâce à "imread()")
+    @param alpha: appartient à [1.0 ; 3.0] représente l'augementation du contraste
+    @param beta: appartient à [0 ; 100] représente l'augmentation du brightness
+    @return: -1 si erreur, l'image traité sinon
+    '''
+
+    if (alpha>=0.0 and alpha<=3.0 and beta>=0 and beta<=100):
+        for i in range(len(img)):
+            for j in range(len(img[0])):
+                img[i][j] = int(alpha * img[i][j] + beta)
+
+    return img
--- Model/segmentationFibre.py
+++ Model/segmentationFibre.py
@@ -4,12 +4,70 @@
 ###########################
 
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
-import preTraitement
+import preTraitementImg
+import time
+import scipy
 
 def fibreSegmentation(path):
     res = cv2.imread(path,0) # Image nuances de gris
-    preTraitement.GB(res,1)
-    preTraitement.HPF(res,1)
+    #res = preTraitementImg.HPF(res,98)
+    res = preTraitementImg.GB(res,1)
+    res = preTraitementImg.BLF(res,1)
+    res = preTraitementImg.contrastAndBrightness(res,1.1,0)
+    for i in range(len(res)):
+        for j in range(len(res[0])):
+            if(res[i][j]>11):
+                res[i][j] = 0
+            else:
+                res[i][j] = 255
+
     return res
 
+def kMeans(img,k):
+    '''
+    Applique la méthode des k-means sur une image
+    @param img: image à traiter (créer précédemment grâce à "imread()")
+    @param k: nombre de clusters
+    @return: l'image après traitement
+    '''
+
+    # convert to np.float32
+    res = img.reshape((-1, 3))
+    res = np.float32(res)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, label, center = cv2.kmeans(res, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res = (res/np.min(res))-1 # normaliser à 0
+    res = scipy.sign(res) # Binarisation
+    res = res * 255 # pour afficher en noir blanc
+    res2 = res.reshape((img.shape))
+
+    return res2
+
+def main(): # Fonction de test
+    start_time = time.time()
+
+    plt.figure(1)
+    plt.subplot(121)
+    img = cv2.imread('Stries_C2  (11).TIF', 0)
+    plt.imshow(img)
+    plt.subplot(122)
+    #imgSeg = fibreSegmentation('Stries_C2  (22).TIF')
+    imgSeg = kMeans(cv2.imread('Stries_C2  (11).TIF',0),10)
+    plt.imshow(imgSeg)
+
+    plt.figure(2)
+    plt.hist(img.ravel(),bins='auto')
+
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+    plt.show()
+
+main()
