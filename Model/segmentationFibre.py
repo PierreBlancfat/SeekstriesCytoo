0d74f2c2e210563c6d07e2ee653715f8ebf55a18
Rospars Quentin <Quentin.Rospars@etu.univ-grenoble-alpes.fr>
2018-04-13 09:43:28 +0200

    Auto stash before merge of "Developpement" and "origin/Developpement"

--- Model/segmentationFibre.py
+++ Model/segmentationFibre.py
@@ -5,4 +5,11 @@
 
 import numpy as np
 import cv2
+import preTraitement
+
+def fibreSegmentation(path):
+    res = cv2.imread(path,0) # Image nuances de gris
+    preTraitement.GB(res,1)
+    preTraitement.HPF(res,1)
+    return res
 
