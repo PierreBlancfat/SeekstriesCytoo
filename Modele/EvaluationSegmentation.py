d67bdfad1cd805a41717ca525c04229f372f73e8
Leduc <polchop@live.fr>
2018-04-17 14:16:42 +0200

    Version 0.6 Fonction d'évaluation de plusieurs images. TODO Améliorer les statistiques, rendre le code plus générique

--- Modele/EvaluationSegmentation.py
+++ Modele/EvaluationSegmentation.py
@@ -4,15 +4,17 @@ import os
 import numpy as np
 from PIL import Image
 import scipy
-
+import Modele.SegmentationGabor
 def evalUneImage(imgRef,imgTest):
     """
     Evaluation de la qualité d'une segmentation 
     :param imgRef: une matrice binaire
     :param imgTest: une matrice binaire
-    :return: recouv : pourcentage de recouvrement
-             erreur : pourcentage d'erreur
+    :return: [precision,prevalence,PPV,FDR,FOR,NPV]
     """
+    print(len(imgRef.shape))
+    if len(imgRef.shape)== 3: # si l'image à plusieurs composantes
+      imgRef = imgRef[:,:,1]
     nbTotal = imgRef.size
     vraiPositif = (imgRef & imgTest)
     inverseRef = inverseMatBin(imgRef)
@@ -26,8 +28,8 @@ def evalUneImage(imgRef,imgTest):
     precision = (CP)/nbTotal
     prevalence = (CN)/ nbTotal
     PPV = np.sum(vraiPositif)/PCP # positive predicted value
-    FDR = np.sum(fauxPositif) / PCP #false discovery rate
-    FOR = np.sum(fauxNegatif) / PCN# false omission rate
+    FDR = np.sum(fauxPositif) / PCP # false discovery rate
+    FOR = np.sum(fauxNegatif) / PCN # false omission rate
     NPV = np.sum(vraiNegatif) / PCN # negative predictive value
     return [precision,prevalence,PPV,FDR,FOR,NPV]
 
@@ -35,21 +37,34 @@ def evalUneImage(imgRef,imgTest):
 def inverseMatBin(mat):
     return (abs(mat - np.ones(mat.shape))).astype(int)
 
-def evalDesImage(srcRef,srcTest):
+def evalDesImages(srcRef,srcTest,algoSegmentation):
     """
-    
-    :param srcRef: 
-    :param srcTest: 
-    :return: 
+    :param srcRef: chemin du dossier reference ( masque binaire)
+    :param srcTest:v chemin du dossier des images test
+    :return: une matrice avec autant de lignes que d'images
     """
-    cheminImagesRef = os.listdir(srcRef)
-    cheminImagesTest = os.listdir(srcTest)
-    nbImage = cheminImagesRef.__sizeof__()
-    result = np.zeros([nbImage,2])
-    for i in range(0,nbImage):
-        imageRef = conversionBinaire(cheminImagesRef[i])
-        #imgTest = cheminImagesRef   # faire tourner les algo de segmentation sur cette image
-        result[i] = evalUneImage(imageRef)
+    nomsImagesRef = os.listdir(srcRef)
+    nomsImagesTest = os.listdir(srcTest)
+    indice = 0
+    result = np.zeros((150,6))
+    print(nomsImagesRef)
+    for imgTest in nomsImagesTest: # pour chaque image à tester
+        imgTest = imgTest[:-4]
+        if any(imgTest in s for s in nomsImagesRef): #si le masque existe
+            print(imgTest)
+
+            indexMasque = nomsImagesRef.index(imgTest+"_s.tif") #recupérer l'index du masque dand la liste
+            cheminImageTest = srcTest+"/"+imgTest+".tif"        # construit le chemin de l'image à tester
+            cheminImageRef = srcRef+"/"+nomsImagesRef[indexMasque] #construit le chemin du masque
+            imgTest = cv2.imread(cheminImageTest)     #lecture de l'image
+            imgRef = np.array(Image.open(cheminImageRef))
+            imgTestSeg = inverseMatBin(algoSegmentation(imgTest))
+            imgRef = conversionBinaire(imgRef)
+           # Image.fromarray((imgTestSeg)).show()
+           #Image.fromarray(imgRef*100+imgTestSeg*100).show()
+            result[indice]  = evalUneImage(imgRef,imgTestSeg)
+            indice+=1
+    print(indice)
     return result
 
 def conversionBinaire(img):
@@ -61,16 +76,19 @@ def conversionBinaire(img):
      imarray = np.array(img) # image to nparray
      imarray = scipy.sign(imarray)  # binarise
      imarray = np.floor(abs(imarray - np.ones(imarray.shape))) #inversion 1 -> 0, 0-> 1
-     imarray = imarray.astype(int)
+     imarray = imarray.astype(int) # converti en int
      return  imarray
 
 
 #main  test
-imgTest = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (15)_fibre.tif")
-imgTest = np.array(imgTest)
-print(imgTest.shape)
-imgTest = conversionBinaire(imgTest)
-imaRef = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (16)_fibre.tif")
-imaRef = np.array(imaRef)
-imaRef = conversionBinaire(imaRef)
-print(evalUneImage(imgTest,imaRef))
+# imgTest = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (26)_fibre.tif")
+# imgTest = np.array(imgTest)
+# imgTest = conversionBinaire(imgTest)
+# imaRef = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (25)_fibre.tif")
+# imaRef = np.array(imaRef)
+# imaRef = conversionBinaire(imaRef)
+# print(evalUneImage(imgTest,imaRef))
+
+tabREsult = evalDesImages("D:/L3MI/2nd_Annee/Cytoo/Stries","D:/L3MI/2nd_Annee/Cytoo/Images_stage_stries",Modele.SegmentationGabor.segmentaionGabor)
+tabREsultSum = np.sum(tabREsult,axis=0)/137
+print(tabREsultSum)
