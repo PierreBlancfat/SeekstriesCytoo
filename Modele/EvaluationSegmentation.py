9f5a17a4dd5dd0cd131ca32cf879dcd94776a445
Leduc <polchop@live.fr>
2018-04-20 14:48:13 +0200

    Version 0.8 Plus de stat, inclusion de la plage de valeur de lambda

--- Modele/EvaluationSegmentation.py
+++ Modele/EvaluationSegmentation.py
@@ -5,6 +5,9 @@ import numpy as np
 from PIL import Image
 import scipy
 import Modele.SegmentationGabor
+from tempfile import TemporaryFile
+
+
 def evalUneImage(imgRef,imgTest):
     """
     Evaluation de la qualité d'une segmentation 
@@ -12,32 +15,57 @@ def evalUneImage(imgRef,imgTest):
     :param imgTest: une matrice binaire
     :return: [precision,prevalence,PPV,FDR,FOR,NPV]
     """
-    print(len(imgRef.shape))
     if len(imgRef.shape)== 3: # si l'image à plusieurs composantes
-      imgRef = imgRef[:,:,1]
+      imgRef = imgRef[:,:,0]
     nbTotal = imgRef.size
-    vraiPositif = (imgRef & imgTest)
-    inverseRef = inverseMatBin(imgRef)
-    fauxPositif = inverseRef & imgTest
-    vraiNegatif = imgRef & inverseMatBin(vraiPositif)
-    fauxNegatif= inverseMatBin(imgTest | imgRef)
-    CP = np.sum(vraiPositif) + np.sum(fauxNegatif)
-    CN = np.sum(fauxPositif) + np.sum(vraiNegatif)
-    PCP = np.sum(abs(vraiPositif)) + np.sum(fauxPositif) # predicted condition positiv
-    PCN = np.sum(abs(vraiNegatif)) + np.sum(fauxNegatif) # predicted condition negative
+    vraiPositifMat = (imgRef & imgTest)
+    inverseRefMat = inverseMatBin(imgRef)
+    fauxPositifMat = inverseRefMat & imgTest
+    vraiNegatifMat = imgRef & inverseMatBin(vraiPositifMat)
+    fauxNegatifMat= inverseMatBin(imgTest | imgRef)
+    vraiPositif = np.sum(vraiPositifMat)
+    fauxPositif =np.sum(fauxPositifMat)
+    vraiNegatif =  np.sum(vraiNegatifMat)
+    fauxNegatif = np.sum(fauxNegatifMat)
+    CP = vraiPositif + fauxNegatif
+    CN = fauxPositif + vraiNegatif
+    PCP = vraiPositif + fauxPositif + 1# predicted condition positiv
+    PCN = vraiNegatif + fauxNegatif + 1 # predicted condition negative
     precision = (CP)/nbTotal
     prevalence = (CN)/ nbTotal
-    PPV = np.sum(vraiPositif)/PCP # positive predicted value
-    FDR = np.sum(fauxPositif) / PCP # false discovery rate
-    FOR = np.sum(fauxNegatif) / PCN # false omission rate
-    NPV = np.sum(vraiNegatif) / PCN # negative predictive value
-    return [precision,prevalence,PPV,FDR,FOR,NPV]
+    PPV = vraiPositif / PCP # positive predicted value
+    FDR = fauxPositif / PCP # false discovery rate
+    FOR = fauxNegatif / PCN # false omission rate
+    NPV = vraiNegatif / PCN # negative predictive value
+    VPV =  vraiNegatif + vraiPositif+1
+    #
+    FNR = 0
+    TPR = 0
+    if(CP != 0):
+        FNR = fauxNegatif / CP # false negative RAte
+        TPR = vraiPositif / CP  #True positive rate
+    #
+    FPR = 0
+    TNR = 0
+    if(CN != 0):
+        FPR = fauxPositif /  CN #False positive Rate
+        TNR = vraiNegatif / CN  #True negative rate
+
+    LRplus =  0
+    LRmoins = 0
+    if FDR != 0:
+         LRplus = TPR/FPR
+    if TNR != 0:
+        LRmoins = FNR/TNR
+    # print([precision,prevalence,PPV,FDR,FOR,NPV,TPR,FPR,FNR,TNR,LRplus,LRmoins])
+    vraivrai = vraiPositif/VPV
+    return [precision,prevalence,PPV,FDR,FOR,NPV,TPR,FPR,FNR,TNR,LRplus,LRmoins,vraivrai]
 
 
 def inverseMatBin(mat):
     return (abs(mat - np.ones(mat.shape))).astype(int)
 
-def evalDesImages(srcRef,srcTest,algoSegmentation):
+def evalDesImages(srcRef,srcTest,algoSegmentation,csize,lsize,thetaMin,thetaMax,pasTheta,sigma,gamma,lambdaMin,lambdaMax,pasLambda,psi):
     """
     :param srcRef: chemin du dossier reference ( masque binaire)
     :param srcTest:v chemin du dossier des images test
@@ -46,26 +74,27 @@ def evalDesImages(srcRef,srcTest,algoSegmentation):
     nomsImagesRef = os.listdir(srcRef)
     nomsImagesTest = os.listdir(srcTest)
     indice = 0
-    result = np.zeros((150,6))
-    print(nomsImagesRef)
+    result = np.zeros((150,13))
     for imgTest in nomsImagesTest: # pour chaque image à tester
         imgTest = imgTest[:-4]
         if any(imgTest in s for s in nomsImagesRef): #si le masque existe
-            print(imgTest)
-
             indexMasque = nomsImagesRef.index(imgTest+"_s.tif") #recupérer l'index du masque dand la liste
             cheminImageTest = srcTest+"/"+imgTest+".tif"        # construit le chemin de l'image à tester
             cheminImageRef = srcRef+"/"+nomsImagesRef[indexMasque] #construit le chemin du masque
             imgTest = cv2.imread(cheminImageTest)     #lecture de l'image
             imgRef = np.array(Image.open(cheminImageRef))
-            imgTestSeg = inverseMatBin(algoSegmentation(imgTest))
+            imgTestSeg = inverseMatBin(algoSegmentation(imgTest,csize,lsize,thetaMin,thetaMax,pasTheta,sigma,gamma,lambdaMin,lambdaMax,pasLambda,psi))
             imgRef = conversionBinaire(imgRef)
-           # Image.fromarray((imgTestSeg)).show()
-           #Image.fromarray(imgRef*100+imgTestSeg*100).show()
-            result[indice]  = evalUneImage(imgRef,imgTestSeg)
+            # if len(imgRef.shape) == 3:  # si l'image à plusieurs composantes
+            #     imgRefS = imgRef[:, :, 0]
+            #     # Image.fromarray(imgRefS*100+imgTestSeg*200).show()
+            #     # print("hic")
+            # else:
+                # Image.fromarray(imgRef * 100 + imgTestSeg * 200).show()
+            #Image.fromarray((imgTestSeg)).show()
+            result[indice] = evalUneImage(imgRef,imgTestSeg)
             indice+=1
-    print(indice)
-    return result
+    return np.sum(result,axis=0)/indice
 
 def conversionBinaire(img):
      """
@@ -80,15 +109,58 @@ def conversionBinaire(img):
      return  imarray
 
 
-#main  test
-# imgTest = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (26)_fibre.tif")
-# imgTest = np.array(imgTest)
-# imgTest = conversionBinaire(imgTest)
-# imaRef = Image.open("D:/L3MI/2nd_Annee/Cytoo/Fibres/Stries_C2  (25)_fibre.tif")
-# imaRef = np.array(imaRef)
-# imaRef = conversionBinaire(imaRef)
-# print(evalUneImage(imgTest,imaRef))
 
-tabREsult = evalDesImages("D:/L3MI/2nd_Annee/Cytoo/Stries","D:/L3MI/2nd_Annee/Cytoo/Images_stage_stries",Modele.SegmentationGabor.segmentaionGabor)
-tabREsultSum = np.sum(tabREsult,axis=0)/137
-print(tabREsultSum)
+
+def evaluationParametreGabor(srcDossierImageRef,srcDossiertest,thetaMin,thetaMax,pasTheta,sigmaMin,sigmaMax,pasSigma,gamaMin,GamaMax,pasGama,lambdaMin,lambdaMax,pasLambda,psiMin,psiMax,pasPsi):
+    """
+    Evalue une plage de paramètres données à la fonction de segmentation de Gabor
+    :return: Une liste donnant les paramètres données et le résulat de l'évaluation 
+    """
+    stat= list()
+    csize = 54
+    lsize = 54
+    for sigma in np.arange(sigmaMin,sigmaMax, pasSigma):
+            for psi in np.arange(psiMin,psiMax,pasPsi):
+                for gamma in np.arange(gamaMin,GamaMax,pasGama):
+                    print(thetaMin.__str__()+" "+thetaMAx.__str__()+" "+pasTheta.__str__()+" "+sigma.__str__()+" "+gamma.__str__()+" "+lambdaMin.__str__()+" "+lambdaMax.__str__()+" "+pasLambda.__str__()+" "+psi.__str__())
+                    reslt = evalDesImages(srcDossierImageRef,srcDossiertest,Modele.SegmentationGabor.segmentationGabor,csize,lsize,thetaMin,thetaMax,pasTheta,sigma,gamma,lambdaMin,lambdaMax,pasLambda,psi)
+                    listReturn = [thetaMin,thetaMax,pasTheta,sigma,gamma,lambdaMin,lambdaMax,pasLambda,psi,reslt.tolist()]
+                    print(reslt)
+                    stat.append(listReturn)
+    return stat
+
+
+#main
+#Choix de la plage de paramètre à tester pour gabor
+
+#Pour boucle de création de kernel*
+#angle
+thetaMin = -0.9
+thetaMAx = 0.9
+pasTheta = 0.3
+#frequence
+lambdaMin = 5
+lambdaMax = 12
+pasLambda = 0.5
+
+# pour variation des paramètres
+#ecart type gaussienne
+sigmaMin = 6.2
+sigmaMax = 7
+pasSigma = 2
+
+#spacial aspect ration
+gamaMin = 1.5
+GamaMax = 2
+pasGama = 2
+
+#décalage
+psiMin = 0
+psiMax = 1
+pasPsi = 2
+
+srcDossierImageRef= "D:/L3MI/2nd_Annee/Cytoo/Stries"
+srcDossiertest=  "D:/L3MI/2nd_Annee/Cytoo/StriesTestPetit"
+
+resultEval = evaluationParametreGabor(srcDossierImageRef,srcDossiertest,thetaMin,thetaMAx,pasTheta,sigmaMin,sigmaMax,pasSigma,gamaMin,GamaMax,pasGama,lambdaMin,lambdaMax,pasLambda,psiMin,psiMax,pasPsi)
+print(resultEval)
