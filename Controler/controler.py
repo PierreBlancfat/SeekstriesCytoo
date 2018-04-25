91f99ac0dacba1e93c01de955c2ebf6c8387b5ba
Vincent <vincent.ribot@free.fr>
2018-04-25 11:10:33 +0200

    remove bad controler

--- Controler/controler.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from Vue.ihm import *
-class Controler():
-
-    def __init__(self):
-        fenetre = Tk()
-        interface = Interface(fenetre)
-        interface.mainloop()
