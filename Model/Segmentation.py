2ab8a1b17eb729a44b7da402e0760c54bc05540a
PierreBlancfat <polchop@live.Fr>
2018-04-26 10:11:38 +0200

    Update Segmentation.py

--- Model/Segmentation.py
+++ Model/Segmentation.py
@@ -4,30 +4,36 @@ from PIL import Image
 from Model.segmentationFibre import SegmentationFibre
 from Model.SegmentationGabor import SegmentationGabor
 from Model.Entourage import Entourage
+import time
 
 class Segmentation:
 
     def __init__(self,cheminSrc,cheminDest):
 
-        self.cheminSrc = cheminSrc # object segmentationFibre
-        self.cheminDest = cheminDest # object segmenation stries
+        self.cheminSrc = "D:/L3MI/2nd_Annee/Cytoo/StriesTestPetit" # object segmentationFibre
+        self.cheminDest = "D:/L3MI/2nd_Annee/Cytoo/testSegGabor/seg/" # object segmenation stries
         self.image = None
 
 
     def segmenterUneImage(self,srcImage,):
+        srcImage = "D:/L3MI/2nd_Annee/Cytoo/Images_stage - Copie/Stries_C2  (44).tif"
         matImg = cv2.imread(srcImage)
-        segFibre = SegmentationFibre()
-        segGabor = SegmentationGabor()
+        # segFibre = SegmentationFibre()
+        segGabor = SegmentationGabor(matImg)
         #maskFibre = segFibre.Segmenter(matImg) #TODO utiliser maskFibre pour économiser la segmentation des stries
-        maskGabor = segGabor.segmentation(matImg)
-        imageEntoure = Entourage.dessinerEntourage(matImg,maskGabor)
-        return imageEntoure
+        maskGabor = segGabor.segmentation()
+        #imageEntoure = Entourage.dessinerEntourage(matImg,maskGabor)
+        return maskGabor
 
     def segmenterDesImages(self):
         nomsImages = os.listdir(self.cheminSrc)
         for img in nomsImages:  # pour chaque image à segmenter
-             cheminImage = self.cheminSrc + "/" + str(img) + ".tif"
+             cheminImage = str(self.cheminSrc) + "/" + str(img) + ".tif"
              imgEntouree = self.segmenterUneImage(cheminImage)
-             Image.fromarray(imgEntouree).save(self.cheminDest)
+             Image.fromarray(imgEntouree).save(self.cheminDest+str(time.time())+".tif")
+
+
+     # def combinerSegmentation(self):
+
 
 
