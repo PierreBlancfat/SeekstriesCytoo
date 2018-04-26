0317e85e36856bd807b82da668ffa1885cc09315
PierreBlancfat <polchop@live.Fr>
2018-04-26 09:17:12 +0200

    Update Controler.py

--- Controler/Controler.py
+++ Controler/Controler.py
@@ -1,24 +1,17 @@
-from Model.Model import *
+from pylint.testutils import INFO_TEST_RGX
 from View.View import *
-from PIL import Image
-from Model.EvaluationSegmentation import EvaluationSegmentation
+from Model.Segmentation import Segmentation
 
-class Controler():
 
-    def __init__(self):
-        frame = Tk()
-        self.model = Model("../Data/images/", "./Data/imageEnregistre")
+frame = Tk()
 
-        '''
-        #Test Vincent entourage
-        image = Image.open("../Data/images/Stries_C2  (8).TIF")
-        matrix = EvaluationSegmentation.conversionBinaire(image)
-        areas = self.model.getCoordStriedArea(matrix)
-        for i in range(0,len(areas)):
-            print(i)
-            print(str(areas[i].xTopLeft) + " " + str(areas[i].yTopLeft) + " " + str(areas[i].xBotRight) + " " + str(areas[i].yBotRight))
-        
-        
-        self.interface = Interface(frame)
-        self.interface.mainloop()
-        '''
+
+def initFenetre():
+    label = Label(frame, text="Hello World")
+    interface = Interface(frame)
+    interface.mainloop()
+
+
+
+if __name__ == '__main__':
+    initFenetre()
