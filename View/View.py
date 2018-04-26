1b7a410c1079e232de0f98c8aac490c71640df76
PierreBlancfat <polchop@live.Fr>
2018-04-26 09:17:56 +0200

    Update View.py

--- View/View.py
+++ View/View.py
@@ -1,5 +1,7 @@
 from tkinter import filedialog
 from tkinter import *
+from Model.Segmentation import Segmentation
+
 
 class Interface(Frame):
 
@@ -7,10 +9,13 @@ class Interface(Frame):
         Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
         self.pack(fill=BOTH)
 
+
         self.panel = PanedWindow()
         self.panel.config(bd=15, relief=RIDGE)
         self.panel.pack(side=TOP)
 
+        #Row 0
+        row = 0
         self.message = Label(self.panel, text="Sélectionnez le répértoire source:")
         self.message.grid(row=0, column=0)
 
@@ -20,8 +25,22 @@ class Interface(Frame):
         self.bouton_browse = Button(self.panel, text="Browse", fg="red", command=self.browse)
         self.bouton_browse.grid(row=0, column=2)
 
-        self.bouton_cliquer = Button(self, text="Start", fg="red")
+        #Row 1
+        self.message = Label(self.panel, text="Sélectionnez le répértoire dest:")
+        self.message.grid(row=1, column=0)
+
+        self.T = Entry(self.panel)
+        self.T.grid(row=1, column=1)
+
+        self.bouton_browse = Button(self.panel, text="Browse", fg="red", command=self.browse)
+        self.bouton_browse.grid(row=1, column=2)
+
+
+
+        #Row 3
+        self.bouton_cliquer = Button(self, text="Start", fg="red",command=self.cliquer)
         self.bouton_cliquer.pack()
+
 
         self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
         self.bouton_quitter.pack()
@@ -31,3 +50,10 @@ class Interface(Frame):
         self.directory = filedialog.askdirectory()
         self.T.delete(0, END)
         self.T.insert(END, self.directory)
+
+    def cliquer(self,cheminScr=None,cheminDest=None):
+        seg = Segmentation(cheminScr,cheminDest)
+        seg.segmenterDesImages()
+
+
+
